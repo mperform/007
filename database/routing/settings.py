33e5d05e0af048ee39f598d4ee2d5d8508e67b62
Yuehan He <mperform@umich.edu>
2024-03-21 17:40:40 -0400

    ,

--- database/routing/settings.py
+++ database/routing/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(-&89ovc&-#6zmot89hja(b4erazem%=45si8gf_29i9)23fme
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['18.117.138.137' 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['18.117.138.137', 'localhost', '127.0.0.1']
 
 
 # Application definition
